c1ee920aa737dc0fea13dacf56d94fb6bb6fd391
d3meter <mark.demeter3@gmail.com>
2023-03-09 00:21:40 +0100

    change Header and start Home

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import "./css/Header.css";
+import Switch from "@mui/material/Switch";
+import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
+import myLogo from "./../imgs/MD_logo.svg";
 
 function Header() {
+  const label = { inputProps: { "aria-label": "Color switch demo" } };
+
   return (
-    <div>Header</div>
-  )
+    <div className="Header">
+      <div className="section-left">
+        <img src={myLogo} alt="logo" />
+      </div>
+      <div className="section-mid">
+        <h1>Mark Demeter</h1>
+        <h2>Web Developer</h2>
+      </div>
+      <div className="section-right">
+        <div className="switch-lang">
+          <p>EN</p>
+          <Switch {...label} defaultChecked color="default" />
+          <p>GER</p>
+        </div>
+        <div className="menu">
+          <MenuRoundedIcon fontSize="large" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React from "react";
+import "./css/Home.css";
+import headerSvg from "./../imgs/md_header2.svg";
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="Home">
+      <div className="svg-div">
+        <img src={headerSvg} alt="page-header" />
+      </div>
+      <div className="section-1">
+        <div className="about">
+          <h1>About Me</h1>
+          <div className="img-container">
+            <img src="" alt="me" />
+          </div>
+          <div className="about-text">
+            <h2>Welcome to my website!</h2>
+            <p>
+              <span>Welcome to my personal website!</span>
+              The purpose of this website is to introduce myself focused on my
+              tech skills and experiences. I’m a former
+              <span>
+                mechanical engineer with designer and project manager
+                experiences
+              </span>
+              who is looking for the opportunity to enter the IT sector as a
+              <span>Software or Web Developer</span>. I’ve lived, learned and
+              <span>worked in Austria for 7 years.</span>
+            </p>
+            <span>
+              I’m always open to learn new technologies incl. backend JAVA, C#
+              etc.
+            </span>
+          </div>
+          <div className="about-shorts">
+            <span>Age:</span>
+            <p>33 |</p>
+            <span>Family:</span>
+            <p>yes, with one and half children |</p>
+            <span>Location:</span>
+            <p>Sankt-Marein bei Graz 🇦🇹 / Székesfehérvár 🇭🇺</p>
+            <br />
+            <span>Hobby:</span>
+            <p>rock climbing, bouldering |</p>
+            <span>Fun fact:</span>
+            <p>
+              I attended the Ninja Warrior Hungary in 2021 and made it to the
+              semi-finals.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div></div>
+      <div></div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
