232193ab88f5ab8da1755ae9ab3ac0ed0703cc66
d3meter <mark.demeter3@gmail.com>
2023-03-09 22:39:38 +0100

    add responsivity

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -19,13 +19,13 @@ function Home() {
             <div className="about-img">
               <img src={fotoMe} alt="me" />
             </div>
-            <div className="about-text">
+            <p className="about-text">
               <h2>Welcome to my website!</h2>
               <p>
-                <span>Welcome to my personal website!</span>{" "}
                 The purpose of this website is to introduce myself focused on my
                 tech skills and experiences. I’m a former
-                <span>{" "}
+                <span>
+                  {" "}
                   mechanical engineer with designer and project manager
                   experiences
                 </span>{" "}
@@ -33,27 +33,35 @@ function Home() {
                 <span>Software or Web Developer</span>. I’ve lived, learned and{" "}
                 <span>worked in Austria for 7 years.</span>
               </p>
+              <br />
               <span>
                 I’m always open to learn new technologies incl. backend JAVA, C#
                 etc.
               </span>
-            </div>
+            </p>
           </div>
           <div className="about-shorts">
-            <span>| Age:</span>
-            <p>33</p>
-            <span>| Family:</span>
-            <p>yes, with one and half children</p>
-            <span>| Location:</span>
-            <p>Sankt-Marein bei Graz 🇦🇹 / Székesfehérvár 🇭🇺</p>
-          </div>
-          <div className="about-shorts">
-            <span>| Hobby:</span>
-            <p>rock climbing, bouldering</p>
-            <span>| Fun fact:</span>
             <p>
-              I attended the Ninja Warrior Hungary in 2021 and made it to the
-              semi-finals.
+              <span>| Age: </span>
+              33
+            </p>
+            <p>
+              <span>| Family: </span>
+              yes, with one and half children
+            </p>
+            <p>
+              <span>| Location: </span>
+              Sankt-Marein bei Graz 🇦🇹 / Székesfehérvár 🇭🇺
+            </p>
+          </div>
+          <div className="about-shorts">
+            <p>
+              <span>| Hobby: </span>
+              rock climbing, bouldering
+            </p>
+            <p>
+              <span>| Fun fact: </span>I attended the Ninja Warrior Hungary in
+              2021 and made it to the semi-finals.
             </p>
           </div>
         </div>
