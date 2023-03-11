9779a76aedd960b3d8d669edf965e3642f1d20b5
d3meter <mark.demeter3@gmail.com>
2023-03-11 15:09:20 +0100

    add section-2 to Home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,23 @@ import "./css/Home.css";
 import headerSvg2 from "./../imgs/md_header2.svg";
 import headerSvg1 from "./../imgs/md_header1.svg";
 import fotoMe from "./../imgs/mark_demeter_sized.JPG";
+import logoCss from "./../imgs/logos/css3.png";
+import logoDocker from "./../imgs/logos/docker.webp";
+import logoFigma from "./../imgs/logos/figma.webp";
+import logoFirebase from "./../imgs/logos/firebase.png";
+import logoGithub from "./../imgs/logos/github.png";
+import logoHtml from "./../imgs/logos/html5.png";
+import logoInkscape from "./../imgs/logos/inkscape.png";
+import logoJs from "./../imgs/logos/js.png";
+import logoMui from "./../imgs/logos/mui.png";
+import logoNodejs from "./../imgs/logos/nodejs.webp";
+import logoPhotoshop from "./../imgs/logos/photoshop.png";
+import logoPostman from "./../imgs/logos/postman.webp";
+import logoReactRouter from "./../imgs/logos/react-router.png";
+import logoReact from "./../imgs/logos/react.png";
+import logoRestApi from "./../imgs/logos/rest-api.webp";
+import logoSass from "./../imgs/logos/sass.png";
+import logoVs from "./../imgs/logos/vs.png";
 
 function Home() {
   return (
@@ -23,20 +40,21 @@ function Home() {
               <h2>Welcome to my website!</h2>
               <p>
                 The purpose of this website is to introduce myself focused on my
-                tech skills and experiences. I’m a former
+                tech skills and expertises. I am a
                 <span>
                   {" "}
-                  mechanical engineer with designer and project manager
-                  experiences
-                </span>{" "}
-                who is looking for the opportunity to enter the IT sector as a{" "}
-                <span>Software or Web Developer</span>. I’ve lived, learned and{" "}
-                <span>worked in Austria for 7 years.</span>
+                  former mechanical engineer with designer, project manager
+                  experiences and improved soft skills
+                </span>
+                . I’ve lived, learned and{" "}
+                <span>worked in Austria for 7 years</span>. Presently I am
+                looking for the opportunity to enter the IT sector as a{" "}
+                <span>Software or Web Developer</span>.
               </p>
               <br />
               <span>
-                I’m always open to learn new technologies incl. backend JAVA, C#
-                etc.
+                I am always open to learn new technologies incl. backend JAVA,
+                C# etc.
               </span>
             </p>
           </div>
@@ -69,7 +87,28 @@ function Home() {
       <div className="svg-div">
         <img src={headerSvg1} alt="page-header" />
       </div>
-      <div></div>
+      <div className="section-2">
+        <h1>Tech <span>Stacks</span></h1>
+        <div className="logo-container">
+          <img src={logoHtml} alt="html5" />
+          <img src={logoCss} alt="css3" />
+          <img src={logoSass} alt="sass" />
+          <img src={logoJs} alt="javascript" />
+          <img src={logoReact} alt="react" />
+          <img src={logoRestApi} alt="rest-api" />
+          <img src={logoNodejs} alt="nodejs" />
+          <img src={logoVs} alt="vs" />
+          <img src={logoGithub} alt="github" />
+          <img src={logoPostman} alt="postman" />
+          <img src={logoDocker} alt="docker" />
+          <img src={logoReactRouter} alt="react-router" />
+          <img src={logoMui} alt="mui" />
+          <img src={logoFirebase} alt="firebase" />
+          <img src={logoFigma} alt="figma" />
+          <img src={logoPhotoshop} alt="photoshop" />
+          <img src={logoInkscape} alt="inkscape" />
+        </div>
+      </div>
     </div>
   );
 }
