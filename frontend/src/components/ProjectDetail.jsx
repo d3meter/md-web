83e710f31d1e428cc8e0c76908d5ad5cd9b95138
d3meter <mark.demeter3@gmail.com>
2023-03-29 11:46:44 +0200

    fix responsivity of ProjectDetail

--- frontend/src/components/ProjectDetail.jsx
+++ frontend/src/components/ProjectDetail.jsx
@@ -8,57 +8,54 @@ function ProjectDetail({ projectDetail }) {
   const [tabOpen, setTabOpen] = useState(false);
 
   return (
-    <div
-      className={tabOpen ? "ProjectDetail open" : "ProjectDetail close"}
-    >
+    <div className={tabOpen ? "ProjectDetail open" : "ProjectDetail close"}>
       <div className="project-content">
         <div className="project-header">
           <div className="project-title">
-          <h1>{projectDetail.name}</h1>
+            <h1>{projectDetail.name}</h1>
           </div>
           <div className="project-links">
-          {projectDetail.webUrl ? (
+            {projectDetail.webUrl ? (
+              <a
+                className="icon-container"
+                href={
+                  projectDetail.webUrl
+                    ? projectDetail.webUrl
+                    : projectDetail.gitUrl
+                }
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={eyeLogo} alt="web" />
+              </a>
+            ) : (
+              <div className="icon-container">
+                <img
+                  src={eyeLogo}
+                  alt="web"
+                  style={{ opacity: 0.3, cursor: "default" }}
+                />
+              </div>
+            )}
             <a
               className="icon-container"
-              href={
-                projectDetail.webUrl
-                  ? projectDetail.webUrl
-                  : projectDetail.gitUrl
-              }
+              href={projectDetail.gitUrl}
               target="_blank"
               rel="noreferrer"
             >
-              <img src={eyeLogo} alt="web" />
+              <img src={githubLogo} alt="github" />
             </a>
-          ) : (
-            <div className="icon-container">
+            <button
+              className="icon-container"
+              onClick={() => setTabOpen((oldValue) => !oldValue)}
+            >
               <img
-                src={eyeLogo}
-                alt="web"
-                style={{ opacity: 0.3, cursor: "default" }}
+                className={tabOpen ? "active" : "passiv"}
+                src={arrowSymbol}
+                alt="down"
               />
-            </div>
-          )}
-          <a
-            className="icon-container"
-            href={projectDetail.gitUrl}
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src={githubLogo} alt="github" />
-          </a>
-          <button
-            className="icon-container"
-            onClick={() => setTabOpen((oldValue) => !oldValue)}
-          >
-            <img
-              className={tabOpen ? "active" : "passiv"}
-              src={arrowSymbol}
-              alt="down"
-            />
-          </button>
+            </button>
           </div>
-      
         </div>
         <div className="project-preview">
           <img
@@ -76,10 +73,11 @@ function ProjectDetail({ projectDetail }) {
             <h3>{projectDetail.status}</h3>
           </div>
           <p className="stacks">#{projectDetail.stacks.join(" #")}</p>
-          <p>{projectDetail.text}</p>
+          <p className="detail-text">{projectDetail.text}</p>
         </div>
         <div className="box-bottom"></div>
       </div>
+      <p className="detail-text-small">{projectDetail.text}</p>
     </div>
   );
 }
